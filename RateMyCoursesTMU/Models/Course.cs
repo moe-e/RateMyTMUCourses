835267e8f1982395ae62127b01ef0e4c6300dca2
Mohamad <m.elharoufi@hotmail.com>
2024-07-14 00:37:16 -0400

    Project creation

---
+++ RateMyCoursesTMU/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿namespace RateMyCoursesTMU.Models
+{
+    public class Course
+    {
+        public string CourseName { get; set; }
+        public string CourseNumber { get; set; }
+        public string CourseDepartment { get; set; }
+        public int CourseId { get; set; }
+
+        public Course(string CourseName, string CourseNumber, string CourseDepartment, int CourseId)
+        {
+            this.CourseName = CourseName;
+            this.CourseNumber = CourseNumber;
+            this.CourseDepartment = CourseDepartment;
+            this.CourseId = CourseId;
+        }
+    }
+}
---
+++ RateMyCoursesTMU/Models/Review.cs
@@ -0,0 +1,27 @@
+﻿namespace RateMyCoursesTMU.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseNumber { get; set; }
+        public float Quality { get; set; }
+        public float Difficulty { get; set; }
+        public string Description { get; set; }
+        public string FinalGrade { get; set; }
+        public string DatePosted { get; set; }
+
+
+        public Review(int ReviewId, string CourseName, string CourseNumber, float Quality, float Difficulty, string Description, string FinalGrade, string DatePosted)
+        {
+            this.ReviewId = ReviewId;
+            this.CourseName = CourseName;
+            this.CourseNumber = CourseNumber;
+            this.Quality = Quality;
+            this.Difficulty = Difficulty;
+            this.Description = Description;
+            this.FinalGrade = FinalGrade;
+            this.DatePosted = DatePosted;
+        }
+    }
+}
