2c5f8d1fca8ec5456cb24e458f7c37cd34dc454f
Mohamad <m.elharoufi@hotmail.com>
2024-07-14 01:39:19 -0400

    Project creation

---
+++ Backend/RateMyTMUCourses/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RateMyTMUCourses.Models;
+
+namespace RateMyTMUCourses.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
---
+++ Backend/RateMyTMUCourses/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RateMyTMUCourses.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
