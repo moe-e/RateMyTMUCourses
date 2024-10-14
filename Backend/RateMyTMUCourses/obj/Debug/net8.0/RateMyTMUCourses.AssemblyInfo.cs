933a82eb144ea73445d66852fa433d6c71aa1678
Mohamad <m.elharoufi@hotmail.com>
2024-10-13 21:46:45 -0400

    Change port for Dockerfile

--- Backend/RateMyTMUCourses/obj/Debug/net8.0/RateMyTMUCourses.AssemblyInfo.cs
+++ Backend/RateMyTMUCourses/obj/Debug/net8.0/RateMyTMUCourses.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RateMyTMUCourses")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4723e2257f5ddd89e2dc9ccea9d2ac67e6b5cc28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a9aa3a371e7b20d51e4feff6372b9247d38c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("RateMyTMUCourses")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RateMyTMUCourses")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
